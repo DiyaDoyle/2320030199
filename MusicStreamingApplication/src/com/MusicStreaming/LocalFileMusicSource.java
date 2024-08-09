f6e67c44abb1d5b8e8dd8626f898106cc540cdf6
Diya <Diya@LAPTOP-8CUI3JP3>
2024-08-09 13:43:14 +0530

    MusicStreaming (Week 4 in lab)

---
+++ MusicStreamingApplication/src/com/MusicStreaming/LocalFileMusicSource.java
@@ -0,0 +1,24 @@
+package com.MusicStreaming;
+
+public class LocalFileMusicSource implements MusicSource{
+	private String filePath;
+	
+	public LocalFileMusicSource(String filepath){
+		this.filePath= filepath;
+		
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("Playing local File: "+filePath);
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		System.out.println("Stoping local File: "+filePath);
+		
+	}
+
+}
---
+++ MusicStreamingApplication/src/com/MusicStreaming/MusicSource.java
@@ -0,0 +1,6 @@
+package com.MusicStreaming;
+
+public interface MusicSource {
+  void play();
+  void stop();
+}
